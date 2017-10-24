82de081dbb90a7db49ae812c33e633e670e2ab7b
chyzh1689 <chyzh1689@163.com>
2017-10-24 20:13:00 +0800

    商户

--- pay-product/src/main/java/com/qh/pay/entity/Merchant.java
+++ pay-product/src/main/java/com/qh/pay/entity/Merchant.java
@@ -11,61 +11,72 @@ import javax.persistence.Id;
 @Entity
 public class Merchant {
 
-    /*** id 主键 ***/
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
-    /*** 商户名 ***/
-    @Column(length = 20)
-    private String username;
-    /*** 名称 ***/
-    @Column(length = 20)
-    private String name;
-    /*** 密钥 ***/
-    @Column(length = 150)
-    private String md5Key;
-    /*** 余额 ***/
-    @Column
-    private BigDecimal balance;
+	/*** id 主键 ***/
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	/*** 商户名 ***/
+	@Column(length = 20)
+	private String username;
+	/*** 名称 ***/
+	@Column(length = 20)
+	private String name;
+	/*** 密钥 ***/
+	@Column(length = 150)
+	private String md5Key;
+	/*** RSA 公钥 ***/
+	@Column(length = 150)
+	private String publicKey;
+	/*** 余额 ***/
+	@Column
+	private BigDecimal balance;
 
-    public Integer getId() {
-        return id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
-    
-    public String getMd5Key() {
-        return md5Key;
-    }
-    
-    public void setMd5Key(String md5Key) {
-        this.md5Key = md5Key;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public BigDecimal getBalance() {
-        return balance;
-    }
+	public String getMd5Key() {
+		return md5Key;
+	}
 
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }
+	public void setMd5Key(String md5Key) {
+		this.md5Key = md5Key;
+	}
+
+	public String getPublicKey() {
+		return publicKey;
+	}
+
+	public void setPublicKey(String publicKey) {
+		this.publicKey = publicKey;
+	}
+
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
 
 }
